b23cb58d6f6d107f64f4a3d70e2cf7a191c2fe7c
Freddied <freddie@creativedude.co.za>
2019-07-11 11:51:31 +0200

    initial commit

---
+++ build/precache-manifest.8b623568ea813953aef598996dc3f428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097fe3ed575de102a84e54111ff30b3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "159572d450c7bcadeff0",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "159572d450c7bcadeff0",
+    "url": "/static/js/main.a8e1e8ea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
