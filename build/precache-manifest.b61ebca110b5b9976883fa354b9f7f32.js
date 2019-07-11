7b37d49368946977dec0045afc9838d93da39342
Freddied <freddie@creativedude.co.za>
2019-07-11 12:48:21 +0200

    gh deploy

---
+++ build/precache-manifest.b61ebca110b5b9976883fa354b9f7f32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa372e0e59bf091230db7649d64357c",
+    "url": "/responsivecheck/index.html"
+  },
+  {
+    "revision": "d25c3f176a9efdafabf7",
+    "url": "/responsivecheck/static/css/main.5470682e.chunk.css"
+  },
+  {
+    "revision": "427153198d4bda2b0aef",
+    "url": "/responsivecheck/static/js/2.123fe850.chunk.js"
+  },
+  {
+    "revision": "d25c3f176a9efdafabf7",
+    "url": "/responsivecheck/static/js/main.b70d7347.chunk.js"
+  },
+  {
+    "revision": "ea9fa93f8691cfffa47b",
+    "url": "/responsivecheck/static/js/runtime~main.9d95377b.js"
+  }
+]);
