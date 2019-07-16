b96d21179d09c6243e007b761f7f8acecfd16b19
Freddied <freddie@creativedude.co.za>
2019-07-16 14:35:19 +0200

    Updates

--- precache-manifest.9617b1f24451f3722e0c94076bf97d7c.js
+++ precache-manifest.9617b1f24451f3722e0c94076bf97d7c.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7231191318dac28aae98969a43516269",
+    "revision": "025af394762511731f3cf723e9dee2bb",
     "url": "/responsivecheck/index.html"
   },
   {
-    "revision": "e8be58671e9bd589b4de",
-    "url": "/responsivecheck/static/css/main.cd428c53.chunk.css"
+    "revision": "e9946404c548c2521fe4",
+    "url": "/responsivecheck/static/css/main.d9246c65.chunk.css"
   },
   {
     "revision": "cd17fbf84a0d7571784e",
     "url": "/responsivecheck/static/js/2.6a8a6027.chunk.js"
   },
   {
-    "revision": "e8be58671e9bd589b4de",
+    "revision": "e9946404c548c2521fe4",
     "url": "/responsivecheck/static/js/main.6ed3a0af.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/responsivecheck/precache-manifest.e9fabc15500f02b1b445e0cf1ee0856e.js"
+  "/responsivecheck/precache-manifest.9617b1f24451f3722e0c94076bf97d7c.js"
 );
 
 self.addEventListener('message', (event) => {
